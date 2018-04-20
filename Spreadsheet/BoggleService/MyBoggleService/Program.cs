972acc9840af39d02ba597789bc88517d1614936
Steen Sia <steensia101@gmail.com>
2018-04-20 14:51:34 -0600

    Ready to start

--- Spreadsheet/BoggleService/MyBoggleService/Program.cs
+++ Spreadsheet/BoggleService/MyBoggleService/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Boggle;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace MyBoggleService
     {
         static void Main(string[] args)
         {
+            HttpStatusCode status;
+            
+            Username name = new Username { Nickname = "Joe" };
+            BoggleService service = new BoggleService();
+            User user = service.CreateUser(name, out status);
+            Console.WriteLine(user.UserToken);
+            Console.WriteLine(status.ToString());
+
+            // This is our way of preventing the main thread from
+            // exiting while the server is in use
+            Console.ReadLine();
         }
     }
 }
