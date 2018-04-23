a2700fa9b4f352d401ad67b8f77d5b96cb4e6790
Haydelj <jcbhaydel@gmail.com>
2018-04-23 14:32:01 -0600

    Added refrence

--- Spreadsheet/BoggleService/MyBoggleService/Program.cs
+++ Spreadsheet/BoggleService/MyBoggleService/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Boggle;
+using CustomNetworking;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
